5ad720dedfd58d39431ac61011ea396e723818f4
root <root@ip-172-31-18-248.me-south-1.compute.internal>
2021-09-19 16:27:38 +0000

    First Commit

---
+++ iPayOAB_encrypt.js
@@ -0,0 +1,5 @@
+var java = require("java");
+java.classpath.push("iPayOABCryptix.jar");
+
+console.log(java.callStaticMethodSync("cryptix.TripleDesOab", "encrypt", "707360962352707360962352", "amt=.100&action=1&responseURL=http://localhost:8080/merchantPlugin/shopping/bankhosted/HostedHttpResult.jsp&errorURL=http://localhost:8080/merchantPlugin/shopping/bankhosted/HostedHttpError.jsp&trackId=596184582&udf1=udf111111111111111111111111111111111111111111&udf2=udf2222222222222222222222222222222222222222222&udf3=udf3333333333333333333333333333333333333333333&udf4=udf444444444444444444444444444444444444444444444&udf5=udf55555555555555555555555555555555555555555555555&currencycode=512&langid=USA&id=ipay707336589914&password=Shopping@123&"));
+
